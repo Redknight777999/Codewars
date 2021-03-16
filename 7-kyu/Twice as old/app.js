7297ec35e0e852af4b8b7fdae25b825a156561aa
AxelC0de <AxelC0de.github@gmail.com>
2021-03-16 13:32:05 +0300

    add: solution

--- 7-kyu/Twice as old/app.js
+++ 7-kyu/Twice as old/app.js
@@ -1,3 +1,6 @@
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-
+	result = Math.abs(dadYearsOld - sonYearsOld * 2)
+	return result
 }
+
+console.log(twiceAsOld(36, 7));
---
+++ 7-kyu/Twice as old/solution.md
@@ -0,0 +1,5 @@
+возраст отца (dadYearsOld) - возраст сына (sonYearsOld), * 2, взятое по модулю
+
+
+
+result = Math.abs(dadYearsOld - sonYearsOld *2)
